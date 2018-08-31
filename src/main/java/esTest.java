ec715aa7ab1ddcbad36ce20b21268fffb7cbbdd1
MartinMa <maang_ouc@163.com>
2018-08-31 14:44:22 +0800

    Add some comments and more operations on elesticSearch

--- src/main/java/esTest.java
+++ src/main/java/esTest.java
@@ -1,7 +1,14 @@
+import org.elasticsearch.action.bulk.BulkProcessor;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.ByteSizeUnit;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.elasticsearch.common.transport.*;
@@ -12,6 +19,8 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 public class esTest {
     public static void main(String[] args) throws IOException {
@@ -35,21 +44,55 @@ public class esTest {
         插入，更新操作
          */
         IndexResponse indexResponse = client.prepareIndex("twitter", "tweet", "15")
-              /*  .setSource(jsonBuilder()
-                                .startObject()
-                                .field("user", "kimchy")
-                                .field("postDate", new Date())
-                                .field("message", "trying out Elasticsearch")
-                                .endObject()
-                )
-                .get()  ;*/.setSource(message, XContentType.JSON).get();
+                .setSource(message, XContentType.JSON).get();
         /*
         查询操作
          */
         GetResponse response = client.prepareGet("twitter", "tweet", "1").get();
+        /*
+        Bulk Api
+        简单来讲的话就是批量进行操作
+         */
 
+        BulkRequestBuilder bulkRequestBuilder = client.prepareBulk();
+        bulkRequestBuilder.add(client.prepareIndex("twitter", "tweet", "12")
+        .setSource(jsonBuilder()
+                    .startObject()
+                        .field("user", "stev")
+                        .field("message", "Bulk API prepareIndex trying").endObject()));
+
+        bulkRequestBuilder.add(client.prepareUpdate("twitter", "tweet", "1")
+        .setDoc(jsonBuilder().startObject()
+                .field("gender", "male")
+                .endObject()));
+
+        BulkResponse bulkResponse = bulkRequestBuilder.get();
+        if (bulkResponse.hasFailures()) {
+            System.out.println("bulk op failed");
+        }
+
+        /*
+        Bulk Processor
+         */
+        BulkProcessor bulkProcessor = BulkProcessor.builder(client,
+                new BulkProcessor.Listener() {
+                    public void beforeBulk(long l, BulkRequest bulkRequest) {
+                        System.out.println(bulkRequest.numberOfActions());
+                    }
+
+                    public void afterBulk(long l, BulkRequest bulkRequest, BulkResponse bulkResponse) {
+                        System.out.println(bulkResponse.hasFailures());
+                    }
+
+                    public void afterBulk(long l, BulkRequest bulkRequest, Throwable throwable) {
+                        System.out.println(throwable.getMessage());
+                    }
+                }).setBulkActions(100)
+                .setBulkSize(new ByteSizeValue(5, ByteSizeUnit.MB))
+                .setFlushInterval(TimeValue.timeValueSeconds(5))
+                .build();
         System.out.println("twitter,tweet,id=1:" + response.toString());
 
-                        client.close();
+        client.close();
     }
 }
